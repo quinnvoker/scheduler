6d2f7bb7c94a3f96e258cec93fecea5e90f8d6eb
Quinn <quinnvoker@gmail.com>
2020-08-31 16:13:51 -0700

    added daylist stories and daylistitem generation

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import DayListItem from './DayListItem';
 
 export default function DayList(props) {
+  const dayListItems = props.days && props.days
+    .map(day => {
+      return (
+        <DayListItem
+          name={day.name}
+          spots={day.spots}
+          selected={day.name === props.day}
+          setDay={props.setDay}
+        />
+      )
+    })
+
   return (
-    <ul></ul>
+    <ul>
+      {dayListItems}
+    </ul>
   );
 }
