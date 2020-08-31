387dc833847d7459fe360984c371583438737a1b
Quinn <quinnvoker@gmail.com>
2020-08-31 16:03:19 -0700

    added daylistitem tests and formatSpots function

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,14 @@ export default function DayListItem(props) {
     'day-list__item--full': props.spots < 1
   });
 
+  const formatSpots = (spots) => {
+    return `${spots ? spots : 'no'} ${spots > 1 || !spots ? 'spots' : 'spot'} remaining`;
+  };
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
