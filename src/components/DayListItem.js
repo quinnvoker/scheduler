487377df574ac7758563f116a25dfd096128fa7f
Quinn <quinnvoker@gmail.com>
2020-08-31 15:51:51 -0700

    added daylistitem styles and class logic

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,15 @@
 import React from "react";
+import classNames from 'classnames';
+import './DayListItem.scss'
 
 export default function DayListItem(props) {
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots < 1
+  });
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{props.spots}</h3>
     </li>
