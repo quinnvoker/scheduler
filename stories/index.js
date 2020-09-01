e2d7fed62181be8b2e7c0e93344bc79c0a7ac0f6
Quinn <quinnvoker@gmail.com>
2020-08-31 18:33:33 -0700

    added appointment stories

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from "components/DayListItem";
 import DayList from 'components/DayList';
 import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
+import Appointment from 'components/Appointment';
 
 storiesOf("Button", module)
   .addParameters({
@@ -127,3 +128,9 @@ storiesOf("InterviewerList", module)
     />
   ));
   
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{name: 'white', value: '#fff', default: true}]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
