d48a528a6cfc5a929fc8228152c196c5266d229e
Quinn <quinnvoker@gmail.com>
2020-09-01 19:06:42 -0700

    now fetching days and appoinments from api reqs

--- src/components/Application.js
+++ src/components/Application.js
@@ -60,13 +60,14 @@ export default function Application(props) {
   });
 
   const setDay = day => setState({...state, day});
-  const setDays = days => setState(prev => ({...prev, days}));
 
   useEffect(() => {
-    axios.get('/api/days')
-      .then(result => {
-        setDays(result.data);
-      })
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments')
+    ]).then(all => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+    });
   }, []);
 
   return (
