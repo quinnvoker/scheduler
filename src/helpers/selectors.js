f496284cc20c573961e8a7c9f24345ad511c0e85
Quinn <quinnvoker@gmail.com>
2020-09-02 07:26:35 -0700

    implemented getInterviewersForDay (tests passing)

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -11,6 +11,19 @@ export function getAppointmentsForDay(state, day) {
     .map(appointment => appointments[appointment]);
 };
 
+export function getInterviewersForDay(state, day) {
+  const {days, interviewers} = state;
+
+  const checkedDay = days.find(currentDay => currentDay.name === day);
+
+  if (!checkedDay || checkedDay.interviewers.length < 1) {
+    return [];
+  }
+
+  return checkedDay.interviewers
+    .map(interviewer => interviewers[interviewer]);
+};
+
 export function getInterview(state, interview) {
   const {interviewers} = state;
   
