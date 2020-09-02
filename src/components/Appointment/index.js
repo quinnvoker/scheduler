bd8d6bc63092d15d502a39579d78e6616ebabcdf
Quinn <quinnvoker@gmail.com>
2020-09-01 20:26:07 -0700

    added empty and show mode logic

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,13 +2,30 @@ import React from 'react';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import useVisualMode from 'hooks/useVisualMode'
 import './styles.scss'
 
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
+const CREATE = 'CREATE';
+
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY)
+
+  const onAdd = () => {
+    transition(CREATE)
+  };
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={onAdd} />}
+      {mode === SHOW && (
+        <Show 
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
     </article>
   );
 };
