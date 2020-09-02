4a85fc7546c64fd077efae48cd63d9c765f71014
Quinn <quinnvoker@gmail.com>
2020-09-01 19:14:37 -0700

    now fetching interviewers with axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -11,6 +11,7 @@ export default function Application(props) {
     day: 'Monday',
     days: [],
     appointments: {},
+    interviewers: []
   });
 
   const setDay = day => setState({...state, day});
@@ -18,9 +19,10 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       axios.get('/api/days'),
-      axios.get('/api/appointments')
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then(all => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, []);
 
