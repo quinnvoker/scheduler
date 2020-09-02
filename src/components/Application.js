71e530ef9210b5f8713864b23ff814b43c9e2e21
Quinn <quinnvoker@gmail.com>
2020-09-02 07:33:12 -0700

    added interviewer list to appointment form

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import Appointment from 'components/Appointment';
 import axios from 'axios';
 
 import "components/Application.scss";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -27,6 +27,8 @@ export default function Application(props) {
   }, []);
 
   const appointments = getAppointmentsForDay(state, state.day);
+  const interviewers = getInterviewersForDay(state, state.day);
+
   const schedule = appointments.map(appointment => {
     const interview = getInterview(state, appointment.interview);
 
@@ -36,6 +38,7 @@ export default function Application(props) {
         id={appointment.id}
         time={appointment.time}
         interview={interview}
+        interviewers={interviewers}
       />
     );
   });
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -25,7 +25,7 @@ export default function Appointment(props) {
       )}
       {mode === CREATE && (
         <Form 
-          interviewers={[]} 
+          interviewers={props.interviewers} 
           onSave={() => console.log('save')} 
           onCancel={back} 
         />
