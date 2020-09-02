50202335089e0fb12c4d8bd94c99a86d372538d0
Quinn <quinnvoker@gmail.com>
2020-09-01 20:29:38 -0700

    added create with cancel logic

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from './Form';
 import useVisualMode from 'hooks/useVisualMode'
 import './styles.scss'
 
@@ -25,6 +26,9 @@ export default function Appointment(props) {
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
+      )}
+      {mode === CREATE && (
+        <Form interviewers={[]} onSave={() => console.log('save')} onCancel={back} />
       )}
     </article>
   );
