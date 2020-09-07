dd7693f870487f3859b247577810e118953542fd
Quinn <quinnvoker@gmail.com>
2020-09-07 14:47:08 -0700

    form tests now passing

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import Button from 'components/Button';
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setName('');
@@ -15,6 +16,15 @@ export default function Form(props) {
     reset();
     props.onCancel();
   }
+
+  function validate(name, interviewer) {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(name, interviewer);
+  }  
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -32,13 +42,14 @@ export default function Form(props) {
             }}
             data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
+          <Button confirm onClick={() => validate(name, interviewer)}>Save</Button>
         </section>
       </section>
     </main>
