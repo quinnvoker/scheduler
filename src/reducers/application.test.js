b8ea4294552e2ac8ade63f0e90daafa85fe19151
Quinn <quinnvoker@gmail.com>
2020-09-08 16:32:55 -0700

    added test for unsupported reaction in reducer

---
+++ src/reducers/application.test.js
@@ -0,0 +1,7 @@
+import reducer from './application'
+
+it('throws an error with an unsupported type', () => {
+  expect(() => reducer({}, {type: 'HAMBURGER'})).toThrowError(
+    /tried to reduce with unsupported action type/i
+  );
+}); 
