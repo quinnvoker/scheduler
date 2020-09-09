aeb91dedaac2030daef8dead6d6317b43433509b
Quinn <quinnvoker@gmail.com>
2020-09-09 13:51:19 -0700

    added first appointments test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,5 +1,26 @@
-describe('Appointment', () => {
-  it('should book an interview', () => {
+describe('Appointments', () => {
+  beforeEach(() => {
+    cy.request('GET', '/api/debug/reset')
 
+    cy.visit('/');
+    cy.contains('Monday');
+  });
+
+  it('should book an interview', () => {
+    cy.get('[alt=Add]')
+      .first()
+      .click();
+    
+    cy.get('[data-testid=student-name-input]')
+      .type('Saitama')
+
+    cy.get('[alt="Sylvia Palmer"]')
+      .click();
+    
+    cy.contains('Save')
+      .click();
+    
+    cy.contains('.appointment__card--show', 'Saitama');
+    cy.contains('.appointment__card--show', 'Sylvia Palmer');
   })
 })
